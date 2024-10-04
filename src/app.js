e299e01d78de7d2b2d604ac2a6a678de01142c2f
Saptak Manna <saptakmanna100@gmail.com>
2024-10-04 18:21:46 +0530

    Add healthcheck endpoint

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use("/api/v1/health-check", healthcheckRouter);
+
+export { app };
