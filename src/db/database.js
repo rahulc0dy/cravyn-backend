12b82a2a04ed64b9fde62a7d5ee9c9e2716a93e8
rahulc0dy <rahulchakraborty337@gmail.com>
2024-10-06 18:35:21 +0530

    EOF newline added, removed comments

--- src/db/database.js
+++ src/db/database.js
@@ -5,12 +5,4 @@ configDotenv();
 
 const sql = neon(process.env.DATABASE_URL);
 
-/* request handler structure */
-// const requestHandler = async (req, res) => {
-//   const result = await sql`SELECT version()`;
-//   const { version } = result[0];
-//   res.writeHead(200, { "Content-Type": "text/plain" });
-//   res.end(version);
-// };
-
 export { sql };
