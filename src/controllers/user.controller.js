539ed2fb645c7b39fc0dc80342efa6e367b06113
Saptak Manna <saptakmanna100@gmail.com>
2024-10-08 12:38:50 +0530

    Manage jwt, cookies and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,28 +1,103 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { sql } from "../db/database.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const generateAccessToken = function (user) {
+  return jwt.sign(
+    {
+      id: user.customer_id,
+      email: user.email_address,
+      phone: user.phone_number,
+      name: user.name,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+const generateRefreshToken = function (user) {
+  return jwt.sign(
+    {
+      id: user.customer_id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 const loginUser = asyncHandler(async (req, res) => {
   const { phoneNumber, password } = req.body;
 
-  if (!phoneNumber) {
-    return res
-      .status(400)
-      .json(new ApiResponse(400, {}, "Phone number is required."));
+  const requiredFields = [
+    { field: phoneNumber, message: "Phone Number is required." },
+    { field: password, message: "Password is required." },
+  ];
+
+  for (const { field, message } of requiredFields) {
+    if (!field) {
+      return res.status(400).json(new ApiResponse(400, {}, message));
+    }
   }
 
-  if (!password) {
+  var user =
+    await sql`SELECT * FROM Customer WHERE phone_number = ${phoneNumber};`;
+
+  const correctPassword = user[0].password;
+
+  const isPasswordCorrect = await bcrypt.compare(password, correctPassword);
+
+  if (!isPasswordCorrect) {
     return res
-      .status(400)
-      .json(new ApiResponse(400, {}, "Password is required."));
+      .status(401)
+      .json(new ApiResponse(401, {}, "Invalid credentials, please try again."));
   }
+
+  const accessToken = generateAccessToken(user[0]);
+  const refreshToken = generateRefreshToken(user[0]);
+
+  const customerId = user[0].customer_id;
+
+  user = await sql`
+    UPDATE Customer
+    SET refresh_token = ${refreshToken}
+    WHERE customer_id = ${customerId}
+    RETURNING *;
+  `;
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  delete user[0].refresh_token;
+  delete user[0].password;
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: user[0],
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully."
+      )
+    );
 });
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, phoneNumber, email, dateOfBirth, password, confirmPassword } =
     req.body;
-
-  console.log(dateOfBirth);
 
   const requiredFields = [
     { field: name, message: "Name is required." },
@@ -41,8 +116,6 @@ const registerUser = asyncHandler(async (req, res) => {
   const existedUser =
     await sql`SELECT * FROM Customer WHERE phone_number = ${phoneNumber};`;
 
-  console.log(existedUser);
-
   if (existedUser.length > 0) {
     return res
       .status(409)
@@ -54,15 +127,18 @@ const registerUser = asyncHandler(async (req, res) => {
     .toISOString()
     .split("T")[0];
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   // Insert new user into the database
   var user;
 
   try {
     user = await sql`
-         INSERT INTO Customer (name, phone_number, email_address, date_of_birth, refresh_token, profile_image_url, password)
-         VALUES (${name}, ${phoneNumber}, ${email}, ${formattedDateOfBirth}, 'some_refresh_token', 'www.cloudinary.com/Souvik', ${password})
-         RETURNING *;
-       `;
+      INSERT INTO Customer (name, phone_number, email_address, date_of_birth, password)
+      VALUES (${name}, ${phoneNumber}, ${email}, ${formattedDateOfBirth}, ${hashedPassword})
+      RETURNING *;
+    `;
+    user = user[0];
   } catch (error) {
     return res
       .status(500)
@@ -87,9 +163,45 @@ const registerUser = asyncHandler(async (req, res) => {
       );
   }
 
+  delete user.refresh_token;
+  delete user.profile_image_url;
+  delete user.password;
+
   return res
     .status(201)
     .json(new ApiResponse(200, user, "User registered successfully."));
 });
 
-export { loginUser, registerUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  try {
+    await sql`
+      UPDATE Customer
+      SET refresh_token = NULL
+      WHERE customer_id = ${req.user.customer_id}
+      RETURNING *;
+    `;
+  } catch (error) {
+    return res
+      .status(500)
+      .json(
+        new ApiResponse(
+          500,
+          { ...error },
+          "Unable to fetch the logged in user."
+        )
+      );
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully."));
+});
+
+export { loginUser, registerUser, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/login").post(loginUser);
 router.route("/register").post(registerUser);
+router.route("/logout").post(verifyJwt, logoutUser);
 
 export default router;
