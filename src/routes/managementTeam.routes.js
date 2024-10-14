4c1f2c213301413b4a7e26e068030eea3d63ac9b
rahulc0dy <rahulchakraborty337@gmail.com>
2024-10-14 14:20:42 +0530

    Removed registration and updation of business and management teams

--- src/routes/managementTeam.routes.js
+++ src/routes/managementTeam.routes.js
@@ -2,10 +2,8 @@ import { Router } from "express";
 import {
   loginManagementTeam,
   logoutManagementTeam,
-  registerManagementTeam,
   refreshAccessToken,
   deleteManagementTeamAccount,
-  updateManagementTeamAccount,
   getManagementTeamAccount,
 } from "../controllers/managementTeam.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -13,13 +11,11 @@ import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/login").post(loginManagementTeam);
-router.route("/register").post(registerManagementTeam);
 router.route("/logout").post(verifyJwt, logoutManagementTeam);
 router.route("/refresh-token").post(refreshAccessToken);
 router
   .route("/")
   .delete(verifyJwt, deleteManagementTeamAccount)
-  .patch(verifyJwt, updateManagementTeamAccount)
   .get(verifyJwt, getManagementTeamAccount);
 
 export default router;
