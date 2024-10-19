c14c50810344172914d62ba23dc6291eb013e40e
Rahul Chakraborty <rahulchakraborty337@gmail.com>
2024-10-19 08:18:50 +0530

    Restaurant CRUD API (#15)
    * Add restaurant functionality
    
    * restaurant management api
    
    * fix minor bugs

---
+++ src/routes/restaurant.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import {
+  addRestaurant,
+  getRestaurant,
+  updateRestaurant,
+  deleteRestaurant,
+  verifyRestaurant,
+} from "../controllers/restaurant.controller.js";
+
+const router = Router();
+
+router
+  .route("/")
+  .get(getRestaurant)
+  .post(addRestaurant)
+  .patch(updateRestaurant)
+  .delete(deleteRestaurant);
+
+router.route("/verify").post(verifyRestaurant);
+
+export default router;
