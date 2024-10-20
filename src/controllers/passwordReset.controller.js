83b6def277fbc0b5abbe350165e357b8532c158c
Mr. 17 <saptakmanna100@gmail.com>
2024-10-20 09:30:11 +0530

    Add verify OTP endpoint (#19)
    * Add verify OTP endpoint
    
    * Add empty line in email body

--- src/controllers/passwordReset.controller.js
+++ src/controllers/passwordReset.controller.js
@@ -50,19 +50,20 @@ const forgotPassword = asyncHandler(async (req, res) => {
   }
 
   if (!user) {
-    return res
-      .status(404)
-      .json(
-        new ApiResponse(
-          404,
-          { reason: "User does not exist", at: "passwordReset.controller.js" },
-          "User with this email does not exist."
-        )
-      );
+    return res.status(404).json(
+      new ApiResponse(
+        404,
+        {
+          reason: "User does not exist",
+          at: "passwordReset.controller.js -> forgotPassword",
+        },
+        "No user found associated with this email."
+      )
+    );
   }
 
   totp.options = {
-    digits: 6,
+    digits: 4,
   };
 
   const token = totp.generate(process.env.OTP_SECRET);
@@ -79,7 +80,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
         new ApiResponse(
           250,
           { mailResponse, user: user[0] },
-          "OTP sent to your email."
+          "The OTP has been sent to your email."
         )
       );
   } catch (error) {
@@ -88,12 +89,69 @@ const forgotPassword = asyncHandler(async (req, res) => {
         503,
         {
           reason: error.message || "error sending email",
-          at: "passwordReset.controller.js > nodemailer.js",
+          at: "passwordReset.controller.js -> nodemailer.js",
         },
-        "Email could not be sent."
+        "An error occurred while sending the email, please try again."
       )
     );
   }
+});
+
+const verifyOtp = asyncHandler(async (req, res) => {
+  const { otp, email } = req.body;
+  const userType = req.query?.userType;
+
+  const requiredFields = [
+    {
+      field: email,
+      message: "Email is required.",
+      reason: "Email is not defined",
+    },
+    {
+      field: otp,
+      message: "OTP is required.",
+      reason: "OTP is not defined",
+    },
+  ];
+
+  for (const { field, message, reason } of requiredFields) {
+    if (!field) {
+      return res.status(400).json(new ApiResponse(400, { reason }, message));
+    }
+  }
+
+  if (!userType) {
+    return res.status(400).json(
+      new ApiResponse(
+        400,
+        {
+          reason: `userType is ${userType}`,
+          at: "passwordReset.controller.js -> verifyOtp",
+        },
+        "User type is missing."
+      )
+    );
+  }
+
+  const hashedOtp = (await getOtp(userType, email))[0].otp;
+  const isOtpCorrect = await bcrypt.compare(otp, hashedOtp);
+
+  if (!isOtpCorrect) {
+    return res.status(400).json(
+      new ApiResponse(
+        400,
+        {
+          reason: "Incorrect otp",
+          at: "passwordReset.controller.js -> verifyOtp",
+        },
+        "OTP is incorrect."
+      )
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "OTP verified successfully."));
 });
 
 const resetPassword = asyncHandler(async (req, res) => {
@@ -125,20 +183,21 @@ const resetPassword = asyncHandler(async (req, res) => {
   }
 
   const hashedOtp = (await getOtp(userType, email))[0].otp;
-  const otpCorrect = await bcrypt.compare(otp, hashedOtp);
+  const isOtpCorrect = await bcrypt.compare(otp, hashedOtp);
   const passwordHash = await bcrypt.hash(password, 10);
 
   try {
-    if (!otpCorrect) {
-      return res
-        .status(400)
-        .json(
-          new ApiResponse(
-            400,
-            { reason: "Incorrect otp", at: "passwordreset" },
-            "OTP is incorrect."
-          )
-        );
+    if (!isOtpCorrect) {
+      return res.status(400).json(
+        new ApiResponse(
+          400,
+          {
+            reason: "Incorrect otp",
+            at: "passwordReset.controller.js -> resetPassword",
+          },
+          "Incorrect OTP, please try again."
+        )
+      );
     }
 
     let user;
@@ -166,12 +225,12 @@ const resetPassword = asyncHandler(async (req, res) => {
         500,
         {
           reason: error.message || "Failed",
-          at: "passwordReset.controller.js->resetPassword",
+          at: "passwordReset.controller.js -> resetPassword",
         },
-        "Failed to update password."
+        "An error occurred while updating password, please try again."
       )
     );
   }
 });
 
-export { forgotPassword, resetPassword };
+export { forgotPassword, verifyOtp, resetPassword };
