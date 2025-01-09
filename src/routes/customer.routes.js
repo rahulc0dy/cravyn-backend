ce110dcb084a05c53a16a7191ecfa3a982a987fa
Mr. 17 <saptakmanna100@gmail.com>
2025-01-09 20:49:56 +0530

    Revert "feat/repeat-order-endpoint (#43)" (#44)
    This reverts commit 8fc5e3d3ba0c2a48453744ab8a84e3da1a91b548.

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -15,7 +15,6 @@ import {
   placeOrder,
   getCustomerOrderHistory,
   cancelOrder,
-  repeatOrder,
 } from "../controllers/customer.controller.js";
 import { verifyUserJwt } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -69,6 +68,5 @@ router
 router.route("/place-order").post(verifyUserJwt, placeOrder);
 router.route("/order-history").get(verifyUserJwt, getCustomerOrderHistory);
 router.route("/cancel-order").post(verifyUserJwt, cancelOrder);
-router.route("/repeat-order").post(verifyUserJwt, repeatOrder);
 
 export default router;
