f6476d44f889b1ad545a952fbc5241c15ae4e00c
rahulc0dy <rahulchakraborty337@gmail.com>
2025-02-09 10:38:43 +0530

    Add prisma migrations

---
+++ prisma/migrations/20250209050120_/migration.sql
@@ -0,0 +1,82 @@
+-- Create uuid extension
+CREATE
+EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('CUSTOMER', 'DELIVERY_PARTNER', 'RESTAURANT_OWNER', 'RESTAURANT_TEAM', 'MANAGEMENT', 'BUSINESS');
+
+-- CreateEnum
+CREATE TYPE "VehicleType" AS ENUM ('BIKE', 'CYCLE');
+
+-- CreateTable
+CREATE TABLE "User"
+(
+    "id"              UUID         NOT NULL DEFAULT uuid_generate_v4(),
+    "email"           TEXT         NOT NULL,
+    "name"            TEXT         NOT NULL,
+    "profileImageUrl" TEXT,
+    "role"            "Role"       NOT NULL,
+    "createdAt"       TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt"       TIMESTAMP(3) NOT NULL,
+    "type"            TEXT         NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Customer"
+(
+    "id"          UUID         NOT NULL DEFAULT uuid_generate_v4(),
+    "dateOfBirth" TIMESTAMP(3) NOT NULL,
+    "phone"       BIGINT       NOT NULL,
+    "userId"      UUID         NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DeliveryPartner"
+(
+    "id"           UUID          NOT NULL DEFAULT uuid_generate_v4(),
+    "phone"        BIGINT        NOT NULL,
+    "availability" BOOLEAN       NOT NULL DEFAULT false,
+    "vehicleType"  "VehicleType" NOT NULL,
+    "userId"       UUID          NOT NULL,
+
+    CONSTRAINT "DeliveryPartner_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RestaurantOwner"
+(
+    "id"                UUID   NOT NULL DEFAULT uuid_generate_v4(),
+    "phone"             BIGINT NOT NULL,
+    "panNumber"         TEXT   NOT NULL,
+    "restaurantOwnerId" UUID   NOT NULL,
+
+    CONSTRAINT "RestaurantOwner_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User" ("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_userId_key" ON "Customer" ("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DeliveryPartner_userId_key" ON "DeliveryPartner" ("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RestaurantOwner_restaurantOwnerId_key" ON "RestaurantOwner" ("restaurantOwnerId");
+
+-- AddForeignKey
+ALTER TABLE "Customer"
+    ADD CONSTRAINT "Customer_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DeliveryPartner"
+    ADD CONSTRAINT "DeliveryPartner_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RestaurantOwner"
+    ADD CONSTRAINT "RestaurantOwner_restaurantOwnerId_fkey" FOREIGN KEY ("restaurantOwnerId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
