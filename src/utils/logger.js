e56784f657c79eeff3f785b6d1755863b4d69464
rahulc0dy <rahulchakraborty337@gmail.com>
2025-02-09 14:10:35 +0530

    remove process exit

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -61,7 +61,6 @@ const logger = createLogger({
 
 process.on("uncaughtException", (err) => {
   logger.error(`Uncaught Exception: ${err.message}`, { stack: err.stack });
-  process.exit(1);
 });
 
 process.on("unhandledRejection", (reason, promise) => {
