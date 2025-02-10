2fddd398c6d9035b8517f4199f444ad550ffb6d0
rahulc0dy <rahulchakraborty337@gmail.com>
2025-02-10 16:06:25 +0530

    refactor(migrations): reset migrations
    ADD: initialisation migration for db functions and triggers

--- prisma/migrations/20250210103019_reset_database/migration.sql
+++ prisma/migrations/20250210103019_reset_database/migration.sql
@@ -1,7 +1,3 @@
--- Create uuid extension
-CREATE
-EXTENSION IF NOT EXISTS "uuid-ossp";
-
 -- CreateEnum
 CREATE TYPE "Role" AS ENUM ('CUSTOMER', 'DELIVERY_PARTNER', 'RESTAURANT_OWNER', 'RESTAURANT_TEAM', 'MANAGEMENT', 'BUSINESS');
 
@@ -18,7 +14,6 @@ CREATE TABLE "User"
     "role"            "Role"       NOT NULL,
     "createdAt"       TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt"       TIMESTAMP(3) NOT NULL,
-    "type"            TEXT         NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -49,10 +44,10 @@ CREATE TABLE "DeliveryPartner"
 -- CreateTable
 CREATE TABLE "RestaurantOwner"
 (
-    "id"                UUID   NOT NULL DEFAULT uuid_generate_v4(),
-    "phone"             BIGINT NOT NULL,
-    "panNumber"         TEXT   NOT NULL,
-    "restaurantOwnerId" UUID   NOT NULL,
+    "id"        UUID   NOT NULL DEFAULT uuid_generate_v4(),
+    "phone"     BIGINT NOT NULL,
+    "panNumber" TEXT   NOT NULL,
+    "userId"    UUID   NOT NULL,
 
     CONSTRAINT "RestaurantOwner_pkey" PRIMARY KEY ("id")
 );
@@ -67,7 +62,7 @@ CREATE UNIQUE INDEX "Customer_userId_key" ON "Customer" ("userId");
 CREATE UNIQUE INDEX "DeliveryPartner_userId_key" ON "DeliveryPartner" ("userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "RestaurantOwner_restaurantOwnerId_key" ON "RestaurantOwner" ("restaurantOwnerId");
+CREATE UNIQUE INDEX "RestaurantOwner_userId_key" ON "RestaurantOwner" ("userId");
 
 -- AddForeignKey
 ALTER TABLE "Customer"
@@ -79,4 +74,4 @@ ALTER TABLE "DeliveryPartner"
 
 -- AddForeignKey
 ALTER TABLE "RestaurantOwner"
-    ADD CONSTRAINT "RestaurantOwner_restaurantOwnerId_fkey" FOREIGN KEY ("restaurantOwnerId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT "RestaurantOwner_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
