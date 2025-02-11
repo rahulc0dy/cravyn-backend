2ddb95f25c2616d0827d6858d82f9fa087448813
Saptak Manna <saptakmanna100@gmail.com>
2025-02-11 14:36:45 +0530

    Refactor error handler to streamline production responses and enhance debugging information

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,29 +1,29 @@
 import { STATUS } from "../constants/statusCodes.js";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const errorHandler = (err, _req, res, _next) => {
-  const isProduction = process.env.NODE_ENV === "production";
+  const statusCode =
+    err.statusCode || STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR;
+  const message = err.message || "Internal Server Error.";
 
   if (isProduction) {
-    res
-      .status(err.statusCode || STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR)
-      .json({
-        success: false,
-        message: err.message || "Internal Server Error.",
-      });
+    res.status(statusCode).json({
+      success: false,
+      message: message,
+    });
   } else {
     console.error(err);
-    res
-      .status(err.statusCode || STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR)
-      .json({
-        success: false,
-        message: err.message || "Internal Server Error.",
-        reason: err.reason || "Reason could not be found.",
-        stack: err.stack,
-        fileStack: err.stack
-          .split("\n")
-          .slice(1, 4)
-          .map((item) => item.trim()),
-      });
+    res.status(statusCode).json({
+      success: false,
+      message: message,
+      reason: err.reason || "Reason could not be found.",
+      reason: err.reason ?? "Reason could not be found.",
+      fileStackTrace: err.stack
+        .split("\n")
+        .slice(1, 4)
+        .map((item) => item.trim()),
+    });
   }
 };
 
