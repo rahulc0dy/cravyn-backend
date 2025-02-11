aa1e6710d6aaa756c87500712083bfb95cdad225
Saptak Manna <saptakmanna100@gmail.com>
2025-02-11 14:33:55 +0530

    Refactor error handler to improve production error responses and logging

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -3,20 +3,28 @@ import { STATUS } from "../constants/statusCodes.js";
 const errorHandler = (err, _req, res, _next) => {
   const isProduction = process.env.NODE_ENV === "production";
 
-  !isProduction && console.error(err);
-
-  res.status(err.statusCode || STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: err.message || "Internal Server Error.",
-    reason: isProduction ? null : err.reason || "Reason could not be found.",
-    stack: isProduction ? null : err.stack,
-    fileStack: isProduction
-      ? null
-      : err.stack
+  if (isProduction) {
+    res
+      .status(err.statusCode || STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR)
+      .json({
+        success: false,
+        message: err.message || "Internal Server Error.",
+      });
+  } else {
+    console.error(err);
+    res
+      .status(err.statusCode || STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR)
+      .json({
+        success: false,
+        message: err.message || "Internal Server Error.",
+        reason: err.reason || "Reason could not be found.",
+        stack: err.stack,
+        fileStack: err.stack
           .split("\n")
           .slice(1, 4)
           .map((item) => item.trim()),
-  });
+      });
+  }
 };
 
 export { errorHandler };
