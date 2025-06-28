b2192d489c142e4f314623cdc377f94da967b3bf
Saptak Manna <saptakmanna100@gmail.com>
2025-06-28 09:52:54 +0530

    feat(socket): refactor connection handling and add chat event registration

---
+++ src/sockets/events/chat.js
@@ -0,0 +1,18 @@
+import { SOCKET_EVENTS } from "../../constants/socketEvents.js";
+import logger from "../../utils/shared/logger.js";
+
+export default function registerChatEvents(socket, _io) {
+  socket.on(SOCKET_EVENTS.CHAT.SEND_MESSAGE, (message) => {
+    logger.info(`Message received: ${message.message}`);
+    // Here you would typically emit the message to other connected clients
+    // socket.broadcast.emit(SOCKET_EVENTS.CHAT.RECEIVE_MESSAGE, message);
+  });
+
+  socket.on(SOCKET_EVENTS.CHAT.TYPING, (data) => {
+    logger.info(`User typing: ${data.username}`);
+    // Here you would typically emit the typing event to other connected clients
+    // socket.broadcast.emit(SOCKET_EVENTS.CHAT.TYPING, data);
+  });
+
+  // Register other events as needed
+}
